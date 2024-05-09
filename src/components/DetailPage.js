a1f813975812cd39d86679352241ecc460863cd8
johnbunnyan <jsk717543@gmail.com>
2024-05-09 10:26:33 +0900

    fix2

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -8,9 +8,10 @@ function DetailPage() {
   const { state } = location; 
   const index = state.index; 
   const [circle, setCircle] = useState(null);
-  // const [clicks, setClicks] = useState(0); // 클릭 횟수 상태 추가
+  const [clicks, setClicks] = useState(0); // 클릭 횟수 상태 추가
 
   useEffect(() => {
+    clicks = 0;
     // 해당 인덱스에 해당하는 원의 정보를 가져옴
     const savedCircles = JSON.parse(localStorage.getItem('circles'));
     const savedClicks = JSON.parse(localStorage.getItem(`circle_${index}_clicks`)); // 클릭 횟수 가져오기
