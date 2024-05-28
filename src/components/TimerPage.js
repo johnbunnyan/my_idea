92a759c1ed2f208ef1a0cbbe61dec094ca5509a3
johnbunnyan <jsk717543@gmail.com>
2024-05-28 09:43:56 +0900

    fix : 상상하기

--- src/components/TimerPage.js
+++ src/components/TimerPage.js
@@ -18,8 +18,9 @@ const TimerPage = () => {
         success: true,
       };
    
-       // 클릭 수를 5 증가시킴
+       // 클릭 수를 증가시킴
        circles[index].clicks = (circles[index].clicks || 0) + 3;
+       circles[index].remainingTime = 100;
 
       circles[index].timerRecords.push(newRecord);
       localStorage.setItem('circles', JSON.stringify(circles));
